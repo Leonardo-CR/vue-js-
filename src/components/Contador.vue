42ebe7315cb06ef3c2fd7aed8cf8bb8c9102f35f
Leonardo Daniel Cruz Rosas <tudiopsss@hotmail.com>
2025-06-24 16:12:33 -0600

    Creacion del componente contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Hola mundo</h1>
+  <p> {{ contador }} </p>
+
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+
+
+</template>
+
+<script setup>
+
+  import { ref } from 'vue';
+
+  const contador = ref(0);
+
+  function incrementar() {
+    contador.value++;
+  };
+
+  function decrementar() {
+    contador.value--;
+  };
+
+
+
+</script>
+
+<style scoped>
+
+  p{
+    color: red;
+  }
+
+</style>
